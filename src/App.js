977a67ad6cbd9f4fc6702b8b970b96c552db567d
Mirfozil <mirfoziln@gmail.com>
2023-12-05 22:28:39 +0500

    finish react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './routes/home/Home';
+import Navigation from './routes/navigation/Navigation';
 
-function App() {
+
+
+const Shop = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      I'm the shop Page
     </div>
-  );
+  )
 }
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  )
+};
 
 export default App;
